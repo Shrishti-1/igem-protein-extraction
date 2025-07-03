a8dc5a5ea05231b724ae0f0cef47b0d0ee4ae98f
aryan90379 <songrimleader@gmail.com>
2025-07-03 20:14:48 +0530

    Your commit message

--- app/page.js
+++ app/page.js
@@ -29,17 +29,23 @@ const Page = () => {
         </Step>
 
         <Step>
-          <h2 className="text-2xl font-semibold mb-2">Step 2</h2>
-
-          <Image
-            src="https://www.purrfectcatgifts.co.uk/cdn/shop/collections/Funny_Cat_Cards_640x640.png?v=1663150894"
-            alt="Funny Cat"
-            width={640}
-            height={620}
-            className="w-full max-w-md h-auto object-cover rounded-xl mt-4"
-          />
-
-          <p className="mt-2">Custom step content!</p>
+           <div className="max-w-4xl mx-auto flex flex-row items-start gap-4 sm:gap-6">
+            <Image
+              src="/doctor1.png"
+              alt="Doctor illustration"
+              width={129}
+              height={129}
+              className="rounded-lg object-contain flex-shrink-0 w-[64px] h-[64px] sm:w-[100px] sm:h-[100px] md:w-[129px] md:h-[129px]"
+              priority
+            />
+            <div className="relative bg-neutral-800 text-white px-4 py-3 rounded-xl shadow-md w-full">
+              <p className="text-sm sm:text-md font-medium leading-relaxed">
+                We want to make a special protein from bacteria to help solve a problem (like making insulin or a specific enzyme).
+              </p>
+              {/* Speech bubble arrow */}
+              <div className="absolute left-[-10px] top-5 w-0 h-0 border-t-8 border-b-8 border-r-8 border-transparent border-r-neutral-800" />
+            </div>
+          </div>
         </Step>
 
         <Step>
